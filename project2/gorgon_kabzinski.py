7b496991743fd5d4b155028b2c3b2749455b66f1
Adam Gorgon <Ag0r@protonmail.com>
2023-05-20 19:15:41 +0200

    dirty: project 2 finished

--- project2/gorgon_kabzinski.py
+++ project2/gorgon_kabzinski.py
@@ -1,16 +1,37 @@
 ### Implement player's strategy. You can compare it with random player
 ### (or some strategy implemented by one of you colleagues)
 ### Time limit per decision 0.01s !!!
+class Player():
+    def __init__(self, name):
+        self.name = name
+        self.cards = None
 
-class YourPlayer(Player):
+    def putCard(self, declared_card):
+        return self.cards[0], self.cards[0]
+
+    def checkCard(self, opponent_declaration):
+        pass
+
+    def getCheckFeedback(self, checked, iChecked, iDrewCards, revealedCard, noTakenCards, log=True):
+        if log: print("Feedback = " + self.name + " : checked this turn = " + str(checked) +
+                      "; I checked = " + str(iChecked) + "; I drew cards = " +
+                      str(iDrewCards) + "; revealed card = " +
+                      str(revealedCard) + "; number of taken cards = " + str(noTakenCards))
+
+    def startGame(self, cards):
+        self.cards = cards
+
+    def takeCards(self, cards_to_take):
+        self.cards = self.cards + cards_to_take
+
+
+class Gorgon_kabzinski(Player):
     def __init__(self, name):
         super().__init__(name)
         self.last_card = None
 
-    ### player's random strategy
     def putCard(self, declared_card):
 
-        ### check if must draw
         if len(self.cards) == 1 and declared_card is not None and self.cards[0][0] < declared_card[0]:
             return "draw"
 
@@ -23,7 +44,6 @@ class YourPlayer(Player):
                 declaration = self.pick_your_card(declaration)
         return card, declaration
 
-    ### randomly decides whether to check or not
     def checkCard(self, opponent_declaration):
         if opponent_declaration in self.cards: return True
         if self.last_card and (self.last_card[0] > opponent_declaration[0] + 1): return True
@@ -31,6 +51,6 @@ class YourPlayer(Player):
 
     def pick_your_card(self, declaration):
         for chosen_card in self.cards:
-            if chosen_card[0] in (declaration[0], declaration[0]+1):
-                 return chosen_card
+            if chosen_card[0] in (declaration[0], declaration[0] + 1):
+                return chosen_card
         return declaration
